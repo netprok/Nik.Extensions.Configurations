cb544635358cdbfc83c4302f592db121d91bf74d
Ehsan Faramarzi <netpro@email.com>
2024-02-17 12:13:27 +0100

    cod cl up

--- Nik.Extensions.Configurations/GlobalUsings.cs
+++ Nik.Extensions.Configurations/GlobalUsings.cs
@@ -1,17 +1,7 @@
-﻿global using Blake3;
-
-global using Microsoft.Extensions.Configuration;
+﻿global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 
-global using Nik.Common.Abstractions;
-global using Nik.Common.Models;
-
 global using System;
-global using System.Collections;
 global using System.Collections.Generic;
-global using System.Globalization;
 global using System.IO;
 global using System.Linq;
-global using System.Reflection;
-global using System.Security.Cryptography;
-global using System.Text;
